4528bd51f21923b38fe086d0cb5613991d789105
at-phuongdo <dophuong13t2@gmail.com>
2018-12-25 21:36:44 +0700

    Update code to init app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'posts#index'
+
+  devise_for :users,
+    path: '',
+    path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
+    controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations'}
+
+  # get '/users/:id', to: 'users#show'
+  # /users/3 -> Users controller, show action, params {id: '3'}
+  resources :users, only: [:index, :show, :update]
+  resources :friends, only: [:create,:destroy]
+
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :photos, only: [:create]
+    resources :likes, only: [:create, :destroy], shallow: true
+    resources :comments, only: [:index, :create, :destroy], shallow: true
+    resources :bookmarks, only: [:create, :destroy], shallow: true
+  end
 end
